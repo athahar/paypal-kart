5521eb6577003e3f585058728f4895550e361494
Athahar <athahar@gmail.com>
2014-01-01 17:56:00 -0800

    add shopping cart / session storage / checkout page

---
+++ controllers/cart.js
@@ -0,0 +1,75 @@
+'use strict';
+
+
+var CartModel = require('../models/cart'),
+	Product = require('../models/productModel');
+
+
+module.exports = function (app) {
+
+	var model = new CartModel();
+
+
+	app.get('/cart', function (req, res) {
+
+        //Retrieve the shopping cart from memory
+        var cart = req.session.cart,
+            displayCart = {items: [], total: 0},
+            total = 0;
+
+        if (!cart) {
+            res.render('result', {result: 'Your cart is empty!'});
+            return;
+        }
+
+        //Ready the products for display
+        for (var item in cart) {
+            displayCart.items.push(cart[item]);
+            total += (cart[item].qty * cart[item].price);
+        }
+        req.session.total = displayCart.total = total.toFixed(2);
+
+        var model =
+        {
+            cart: displayCart
+        };
+
+        res.render('cart', model);
+    });
+
+	app.post('/cart', function (req, res) {
+
+		//Load (or initialize) the cart
+		req.session.cart = req.session.cart || {};
+		var cart = req.session.cart;
+
+		//Read the incoming product data
+		var id = req.param('item_id');
+
+		//Locate the product to be added
+		Product.findById(id, function (err, prod) {
+			if (err) {
+				console.log('Error adding product to cart: ', err);
+				res.redirect('/cart');
+				return;
+			}
+
+			//Add or increase the product quantity in the shopping cart.
+			if (cart[id]) {
+				cart[id].qty++;
+			}
+			else {
+				cart[id] = {
+					name: prod.name,
+					price: prod.price,
+					prettyPrice: prod.prettyPrice(),
+					qty: 1
+				};
+			}
+
+			//Display the cart for the user
+			res.redirect('/cart');
+
+		});
+	});
+};
